57eb1471fe0752a0825c7461a1d7af79c91871c2
cog-vaibhav <vaibhav.ghanekar@cognizant.com>
2025-06-27 11:24:24 +0530

    First commit

---
+++ src/test/java/com/cognizant/jenkins/test1.java
@@ -0,0 +1,9 @@
+package com.cognizant.jenkins;
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void test1() {
+		System.out.println("This is test1");
+	}
+}
---
+++ src/test/java/com/cognizant/jenkins/test2.java
@@ -0,0 +1,9 @@
+package com.cognizant.jenkins;
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void test2() {
+		System.out.println("This is test2");
+	}
+}
